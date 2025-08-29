ec87a1c9a1643bccdfa0874303c317eb0b52e65c
d_flex <hiyoo95@gmail.com>
2025-08-29 20:14:58 +1000

    features/init-setup : jobService, jobController, jobModel, jobRouter added

---
+++ server/src/routes/jobsRouter.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {authenticate, authorize} = require('../middleware/auth');
+const jobController = require('../controllers/jobController');
+
+const router = express.Router();
+
+router.use(authenticate);
+
+router.get('/', jobController.getAllJobs);
+router.post('/', jobController.createJob);
+router.get('/stats', jobController.getUserStats);
+// load testing purpose
+router.post('/process-sample', jobController.processSample);
+
+
+// video processing
+router.get('/:id', jobController.getJobById);
+router.get('/:id', jobController.updateJob);
+router.delete('/:id', jobController.deleteJob);
+router.get('/:id/assets', jobController.getJobAssets);
+
+module.exports = router;
--- server/src/routes/trailerRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router();
-
-router.get('/', getAll);
-router.get('/:id', getVideoById);
-router.post('/', uploadVideo);
-router.put('/', editVideo);
-router.delete('/', deleteVideo);
