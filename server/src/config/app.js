5dcb225319d16a554efa17625b0366ec0daea161
d_flex <hiyoo95@gmail.com>
2025-08-31 23:10:57 +1000

    docker build

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -19,7 +19,7 @@ class App {
 
       this.app.use(
         cors({
-          origin: ['http://13.210.68.108:3000', 'http://localhost:3000'],
+          origin: ['http://16.176.228.63:3000', 'http://localhost:3000'],
           credentials: true,
         })
       );
